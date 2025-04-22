21ebab4f6d27118f4bf5aac60d5f10b498b23391
Oumaima <oumaima.haddar.majdeb@gmail.com>
2025-04-22 20:29:05 +0200

    ✨ remove navigation items from Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,6 @@ import React from "react";
 import Image from "next/image";
 
 const Navbar: React.FC = () => {
-  const items = [
-    { label: "Compétences", id: "skills" },
-    { label: "Projets", id: "projects" },
-    { label: "Témoignage", id: "testimonials" },
-    { label: "Contact", id: "contact" },
-  ];
-
   return (
     <nav className="bg-gray-800 fixed top-0 w-full z-50 shadow-lg">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
@@ -22,18 +15,6 @@ const Navbar: React.FC = () => {
           />
           <h1 className="text-xl font-bold text-white">Oumaima HADDAR</h1>
         </div>{" "}
-        <ul className="flex space-x-6">
-          {items.map((item) => (
-            <li key={item.id}>
-              <a
-                href={`#${item.id}`}
-                className="text-white hover:text-teal-400 transition-colors"
-              >
-                {item.label}
-              </a>
-            </li>
-          ))}
-        </ul>
       </div>
     </nav>
   );

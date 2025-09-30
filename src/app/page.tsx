ae78dd5625a9f09bca7307117455a76cd1915ddc
Oumaima <oumaima.haddar.majdeb@gmail.com>
2025-09-30 11:13:51 +0200

    ✨ refactor Home component: streamline layout by removing unnecessary sections and imports for improved readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,23 @@
-import Head from "next/head";
-import Navbar from "@/components/Navbar";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Experience from "@/components/Experience";
+import Formation from "@/components/Formation";
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-import About from "@/components/about";
-import TechSkills from "@/components/techSkills";
-import Projects from "@/components/projects";
-import { Experience } from "@/components/Experience";
-import { Education } from "@/components/Education";
 
 export default function Home() {
   return (
-    <div className="bg-gray-900 text-white">
-      <Head>
-        <title>Oumaima HADDAR</title>
-        <meta name="description" content="Portfolio de développeur junior" />
-      </Head>
-
-      <Navbar />
-
-      <main className="container mx-auto px-6 py-38">
-        <section id="parcours">
-          <About />
-        </section>
-
-        <section id="skills">
-          <TechSkills />
-        </section>
-        <section id="experience">
-          <Experience />
-        </section>
-        <section id="formation">
-          <Education />
-        </section>
-        <section id="projects">
-          <Projects />
-        </section>
-      </main>
-
+    <main className="relative">
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Formation />
+      <Contact />
       <Footer />
-    </div>
+    </main>
   );
 }
